e60d29d3feefe69e3d7d3573eafc6f5447bea556
Karl <kdirzys@NN121-HA-22.HS.sd203.org>
2015-10-01 10:30:25 -0500

    string parsing

---
+++ Chapter 4/stringparsing.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class stringparsing
+{
+ 
+    /**
+     * Default constructor for objects of class stringparsing
+     */
+    public static void main(String[] args)
+    {
+        // initialise instance variables
+        Scanner scan = new Scanner(System.in);
+        String number = new String();
+        System.out.print("Enter a number between 1,000 and 999,999 ");
+        number = scan.next();
+        
+    }
+
+
+}
