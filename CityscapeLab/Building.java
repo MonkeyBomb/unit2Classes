3720e403976fb24d09bb32d76d6ad13e3e58b188
Karl <kdirzys@NN121-HA-22.HS.sd203.org>
2015-10-02 10:31:05 -0500

    creating the landscape

---
+++ CityscapeLab/Building.java
@@ -0,0 +1,24 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+public class Building
+{
+  public void main(String[] args)
+  {
+      
+  }
+  public Building(int x, int y)
+  {
+    //position of the building
+    
+      
+  }
+  public void draw(Graphics2D g2)
+  {
+      //drawing instructions
+   }
+}
--- CityscapeLab/CityscapeComponent.java
+++ CityscapeLab/CityscapeComponent.java
@@ -1,7 +1,10 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JComponent;
-
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 /**
  * Class that creates instances of the classes that comprise the cityscape and delegates drawing the
  *  cityscape to these object.
@@ -19,7 +22,6 @@ public class CityscapeComponent extends JComponent
     // define the CityscapeComponent contructor and intiailize all instance variables
     // ...
     
-    
     /**
      * This method is invoked by the Java Run-Time whenever the component needs to be redrawn.
      * It does not need to be invoked explicitly.
@@ -27,8 +29,14 @@ public class CityscapeComponent extends JComponent
      */
     public void paintComponent(Graphics g)
     {
+       
         Graphics2D g2 = (Graphics2D) g;
-        
+        Building build1 = new Building(0,0);
+        int x = getWidth() - 80;
+        int y = getHeight() - 30;
+        Building build2 = new Building(x,y);
+        build1.draw(g2);
+        build2.draw(g2);
         // invoke the draw method on each object in your Cityscape
         // ...
         
